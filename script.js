f558709dbb4f31616682a3fe98554ffd13460489
Lucas Lima <robertolucas011@gmail.com>
2021-12-19 09:25:36 -0300

    Adiciona o primeiro requisito, criando dinâmicamente o título e adicionando o texto a ele.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
 const createHeader = document.createElement('header');
+createHeader.id = 'takeHeader';
 document.body.appendChild(createHeader);
+const createH1 = document.createElement('h1');
+createH1.innerText = 'Paleta de Cores';
+createH1.id = 'title';
+createHeader.appendChild(createH1);
+
+
 
