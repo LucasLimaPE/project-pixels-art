6c2cd2a61416d64abc31b6bd3a51bbb1cd1e4c3f
LucasLimaPE <robertolucas011@gmail.com>
2023-03-27 12:27:52 -0300

    refactor: separa as resonsabilidades de cada arquivo para criar a paleta.

--- script.js
+++ script.js
@@ -1,104 +1,104 @@
 // Desenvolvi este projeto com a ajuda dos colegas Guilherme Azevedo, Imar Mendes e Tiago Zardo.
 
-const createSection = document.createElement('section');
+// const createSection = document.createElement('section');
 
-createSection.id = 'color-palette';
+// createSection.id = 'color-palette';
 
-document.body.appendChild(createSection);
+// document.body.appendChild(createSection);
 
-const createColorPalette = ['black', 'orange', 'green', 'yellow'];
+// const createColorPalette = ['black', 'orange', 'green', 'yellow'];
 
-const createButton = document.createElement('button');
+// const createButton = document.createElement('button');
 
-createButton.id = 'clear-board';
+// createButton.id = 'clear-board';
 
-createButton.innerText = 'Limpar';
+// createButton.innerText = 'Limpar';
 
-document.body.appendChild(createButton);
+// document.body.appendChild(createButton);
 
-for (let index = 0; index < createColorPalette.length; index += 1) {
-  const createDiv = document.createElement('div');
-  createDiv.className = 'color';
-  createDiv.style.borderWidth = '1px';
-  createDiv.style.borderStyle = 'solid';
-  createDiv.style.borderColor = 'black';
-  createDiv.style.backgroundColor = createColorPalette[index];
-  createDiv.style.display = 'inline-block';
-  createDiv.style.width = '40px';
-  createDiv.style.height = '40px';
-  createSection.appendChild(createDiv);
-}
+// for (let index = 0; index < createColorPalette.length; index += 1) {
+//   const createDiv = document.createElement('div');
+//   createDiv.className = 'color';
+//   createDiv.style.borderWidth = '1px';
+//   createDiv.style.borderStyle = 'solid';
+//   createDiv.style.borderColor = 'black';
+//   createDiv.style.backgroundColor = createColorPalette[index];
+//   createDiv.style.display = 'inline-block';
+//   createDiv.style.width = '40px';
+//   createDiv.style.height = '40px';
+//   createSection.appendChild(createDiv);
+// }
 
-const takeCreateDiv = document.querySelectorAll('.color');
+// const takeCreateDiv = document.querySelectorAll('.color');
 
-takeCreateDiv[0].classList.add('selected');
+// takeCreateDiv[0].classList.add('selected');
 
-const createH1dois = document.createElement('h1');
+// const createH1dois = document.createElement('h1');
 
-createH1dois.innerText = 'Quadro de Pixels';
+// createH1dois.innerText = 'Quadro de Pixels';
 
-createH1dois.id = 'quadroPixels';
+// createH1dois.id = 'quadroPixels';
 
-document.body.appendChild(createH1dois);
+// document.body.appendChild(createH1dois);
 
-const createSection2 = document.createElement('section');
+// const createSection2 = document.createElement('section');
 
-createSection2.id = 'pixel-board';
+// createSection2.id = 'pixel-board';
 
-document.body.appendChild(createSection2);
+// document.body.appendChild(createSection2);
 
-for (let index = 0; index < 5; index += 1) {
-  const createDivMae = document.createElement('div');
-  const takeSection = document.querySelector('#pixel-board');
-  for (let index = 0; index < 5; index += 1) {
-    const createDivChild = document.createElement('div');
-    createDivChild.style.marginLeft = '-1px';
-    createDivChild.style.marginTop = '-5px';
-    createDivChild.style.backgroundColor = 'white';
-    createDivChild.style.border = '1px solid black';
-    createDivChild.style.width = '40px';
-    createDivChild.style.height = '40px';
-    createDivChild.style.display = 'inline-block';
-    createDivChild.classList.add('pixel');
-    createDivMae.appendChild(createDivChild);
-    takeSection.appendChild(createDivMae);
-  }
-}
+// for (let index = 0; index < 5; index += 1) {
+//   const createDivMae = document.createElement('div');
+//   const takeSection = document.querySelector('#pixel-board');
+//   for (let index = 0; index < 5; index += 1) {
+//     const createDivChild = document.createElement('div');
+//     createDivChild.style.marginLeft = '-1px';
+//     createDivChild.style.marginTop = '-5px';
+//     createDivChild.style.backgroundColor = 'white';
+//     createDivChild.style.border = '1px solid black';
+//     createDivChild.style.width = '40px';
+//     createDivChild.style.height = '40px';
+//     createDivChild.style.display = 'inline-block';
+//     createDivChild.classList.add('pixel');
+//     createDivMae.appendChild(createDivChild);
+//     takeSection.appendChild(createDivMae);
+//   }
+// }
 
-const takeDivChild = document.querySelectorAll('.pixel');
+// const takeDivChild = document.querySelectorAll('.pixel');
 
-const takeCreateDiv0 = takeCreateDiv[0];
+// const takeCreateDiv0 = takeCreateDiv[0];
 
-const takeCreateDiv1 = takeCreateDiv[1];
+// const takeCreateDiv1 = takeCreateDiv[1];
 
-const takeCreateDiv2 = takeCreateDiv[2];
+// const takeCreateDiv2 = takeCreateDiv[2];
 
-const takeCreateDiv3 = takeCreateDiv[3];
+// const takeCreateDiv3 = takeCreateDiv[3];
 
-function changeColor2(event) {
-  const takeSelcted = document.querySelector('.selected');
-  takeSelcted.classList.remove('selected');
-  event.target.classList.add('selected');
-}
-//
-takeCreateDiv0.addEventListener('click', changeColor2);
+// function changeColor2(event) {
+//   const takeSelcted = document.querySelector('.selected');
+//   takeSelcted.classList.remove('selected');
+//   event.target.classList.add('selected');
+// }
+// //
+// takeCreateDiv0.addEventListener('click', changeColor2);
 
-takeCreateDiv1.addEventListener('click', changeColor2);
+// takeCreateDiv1.addEventListener('click', changeColor2);
 
-takeCreateDiv2.addEventListener('click', changeColor2);
+// takeCreateDiv2.addEventListener('click', changeColor2);
 
-takeCreateDiv3.addEventListener('click', changeColor2);
+// takeCreateDiv3.addEventListener('click', changeColor2);
 
-function changeColor3(event) {
-  event.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
-}
+// function changeColor3(event) {
+//   event.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+// }
 
-for (let index = 0; index < takeDivChild.length; index += 1) {
-  takeDivChild[index].addEventListener('click', changeColor3);
-}
+// for (let index = 0; index < takeDivChild.length; index += 1) {
+//   takeDivChild[index].addEventListener('click', changeColor3);
+// }
 
-createButton.addEventListener('click', () => {
-  for (let index = 0; index < takeDivChild.length; index += 1) {
-    takeDivChild[index].style.backgroundColor = 'white';
-  }
-});
+// createButton.addEventListener('click', () => {
+//   for (let index = 0; index < takeDivChild.length; index += 1) {
+//     takeDivChild[index].style.backgroundColor = 'white';
+//   }
+// });
